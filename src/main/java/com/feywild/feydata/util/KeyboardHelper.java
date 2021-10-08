9b71a132deff32313c7a97f1860f7ea38499db83
SaphieNyako <77398437+SaphieNyako@users.noreply.github.com>
2021-10-08 10:56:26 +0200

    Revert "slabs, stairs, fences, gates"
    This reverts commit f4c3a5bd251dfb8692e8faec1cddf5d9ae178a90.

--- src/main/java/com/feywild/feydata/util/KeyboardHelper.java
+++ src/main/java/com/feywild/feydata/util/KeyboardHelper.java
@@ -10,6 +10,16 @@ public class KeyboardHelper {
 
     @OnlyIn(Dist.CLIENT)
     public static boolean isHoldingShift() {
-        return InputMappings.isKeyDown(Minecraft.getInstance().getWindow().getWindow(), Minecraft.getInstance().options.keyShift.getKey().getValue());
+
+        return InputMappings.isKeyDown(Minecraft.getInstance().getWindow().getWindow(),
+                GLFW.GLFW_KEY_LEFT_SHIFT);
     }
+
+    @OnlyIn(Dist.CLIENT)
+    public static boolean isHoldingCtrl() {
+
+        return InputMappings.isKeyDown(Minecraft.getInstance().getWindow().getWindow(),
+                GLFW.GLFW_KEY_LEFT_CONTROL);
+    }
+
 }
