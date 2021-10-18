7619dcde8558e8e77fe8882d1645817b440d88e1
SaphieNyako <77398437+SaphieNyako@users.noreply.github.com>
2021-10-18 10:41:28 +0200

    Revert "So this should work?"
    This reverts commit 99cdabfcfb04f58b7d055b5adbba22f99a19a53b.

--- src/main/java/com/feywild/feydata/FeyDataMod.java
+++ src/main/java/com/feywild/feydata/FeyDataMod.java
@@ -1,9 +1,6 @@
 package com.feywild.feydata;
 
 import com.feywild.feydata.item.ModItems;
-import com.feywild.feydata.quest.task.BiomeTask;
-import com.feywild.feydata.quest.task.StructureTask;
-import com.feywild.feywild.quest.task.TaskTypes;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.ItemGroup;
@@ -12,7 +9,6 @@ import net.minecraft.loot.ItemLootEntry;
 import net.minecraft.loot.LootEntry;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTables;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.LootTableLoadEvent;
 import net.minecraftforge.event.RegistryEvent;
@@ -70,10 +66,6 @@ public class FeyDataMod {
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
-
-        TaskTypes.register(new ResourceLocation(FeyDataMod.MOD_ID, "biome"), BiomeTask.INSTANCE);
-        TaskTypes.register(new ResourceLocation(FeyDataMod.MOD_ID, "biome"), StructureTask.INSTANCE);
-
     }
 
     private void setup(final FMLCommonSetupEvent event)
